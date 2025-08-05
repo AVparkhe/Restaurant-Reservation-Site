25892431007135d351e5ca786af17bf443044160
AVparkhe <akankshaparkhe53@gmail.com>
2025-08-05 16:29:59 +0530

    Add files via upload

---
+++ Restaurant_Reservation_Project/frontend/src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              We are committed to excellence, offering reliable and customizable options to ensure customer satisfaction. Our team works tirelessly to overcome challenges and deliver solutions that are efficient, honest, and effective. We take pride in our ability to adapt, improve, and respond to feedback, ensuring that every experience is valuable and trustworthy.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
