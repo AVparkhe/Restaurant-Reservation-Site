37f3bb0ff77f3022f311c82fdb7d504cc2b3a010
AVparkhe <akankshaparkhe53@gmail.com>
2025-08-10 16:05:37 +0530

    Update Reservation.jsx

--- Restaurant_Reservation_Project/frontend/src/components/Reservation.jsx
+++ Restaurant_Reservation_Project/frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://restaurant-reservation-dvo7.onrender.com/api/v1/reservation/send",
+        "http://localhost:4000/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
@@ -109,6 +109,7 @@ const Reservation = () => {
 };
 
 export default Reservation;
+
 
 
 
