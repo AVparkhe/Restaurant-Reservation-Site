f6733c933acff0b7427ccd03936893797b2c1669
AVparkhe <akankshaparkhe53@gmail.com>
2025-08-10 17:21:07 +0530

    Update Reservation.jsx

--- Restaurant_Reservation_Project/frontend/src/components/Reservation.jsx
+++ Restaurant_Reservation_Project/frontend/src/components/Reservation.jsx
@@ -11,7 +11,7 @@ const Reservation = () => {
   const [email, setEmail] = useState("");
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
-  const [phone, setPhone] = useState(0);
+  const [phone, setPhone] = useState("");
   const navigate = useNavigate();
 
   const handleReservation = async (e) => {
@@ -109,6 +109,7 @@ const Reservation = () => {
 };
 
 export default Reservation;
+
 
 
 
